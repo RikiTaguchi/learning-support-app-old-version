77ca516f7f90a91f9981c44e2cae1796010f4855
rikitaguchi <riki_iphone2@icloud.com>
2024-12-02 12:11:21 +0900

    set localhost for db

--- source.php
+++ source.php
@@ -1,9 +1,15 @@
 <?php
-// DB関連
+// DB関連(AWS環境)
 $db_user = 'root';
 $db_pass = 'myrdspassword';
 $db_host = 'wordsystemdb.c74wk6yq0kc9.ap-northeast-1.rds.amazonaws.com';
 $db_name = 'wordsystemdb';
+
+// DB関連(ローカル環境)
+// $db_user = 'root';
+// $db_pass = '';
+// $db_host = 'localhost';
+// $db_name = 'wordsystemdb';
 
 // ユーザー情報
 $login_id = $_POST['login_id'];
