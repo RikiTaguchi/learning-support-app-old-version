9d904439fddb667a5456a1a5a622bb2dd0693ef9
rikitaguchi <riki_iphone2@icloud.com>
2024-12-07 02:09:33 +0900

    edit db controller

--- detail_set.php
+++ detail_set.php
@@ -5,7 +5,7 @@ include('./info_db.php');
 $table_id = $_POST['table_id'];
 $book_name = $_POST['book_name'];
 $book_id = $_POST['book_id'];
-$question_number = $_POST['question_number'];
+$question_number = (int)$_POST['question_number'];
 $new_word = $_POST['new_word'];
 $new_answer = $_POST['new_answer'];
 $word = $_POST['word'];
@@ -41,6 +41,31 @@ try {
         $stmt->bindParam(':book_id', $book_id, PDO::PARAM_STR);
         $stmt->bindParam(':question_number', $question_number, PDO::PARAM_INT);
         $stmt->execute();
+        $sql = 'DELETE FROM info_feedback WHERE table_id = :table_id AND book_id = :book_id AND question_number = :question_number';
+        $stmt = $dbh->prepare($sql);
+        $stmt->bindParam(':table_id', $table_id, PDO::PARAM_INT);
+        $stmt->bindParam(':book_id', $book_id, PDO::PARAM_STR);
+        $stmt->bindParam(':question_number', $question_number, PDO::PARAM_INT);
+        $stmt->execute();
+        $sql = 'SELECT * FROM info_feedback WHERE table_id = :table_id AND book_id = :book_id ORDER BY question_number';
+        $stmt = $dbh->prepare($sql);
+        $stmt->bindParam(':table_id', $table_id, PDO::PARAM_INT);
+        $stmt->bindParam(':book_id', $book_id, PDO::PARAM_STR);
+        $stmt->execute();
+        $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+        foreach ($result as $row) {
+            if ((int)$row['question_number'] > $question_number) {
+                $question_number_pre = (int)$row['question_number'];
+                $question_number_new = (int)$row['question_number'] - 1;
+                $sql = 'UPDATE info_feedback SET question_number = :question_number_new WHERE table_id = :table_id AND book_id = :book_id AND question_number = :question_number_pre';
+                $stmt = $dbh->prepare($sql);
+                $stmt->bindParam(':table_id', $table_id, PDO::PARAM_INT);
+                $stmt->bindParam(':book_id', $book_id, PDO::PARAM_STR);
+                $stmt->bindParam(':question_number_pre', $question_number_pre, PDO::PARAM_INT);
+                $stmt->bindParam(':question_number_new', $question_number_new, PDO::PARAM_INT);
+                $stmt->execute();
+            }
+        }
     } else if ($set_type == 'delete_all') { // 全削除
         $sql = 'DELETE FROM info_my_book_index WHERE table_id = :table_id AND book_id = :book_id';
         $stmt = $dbh->prepare($sql);
